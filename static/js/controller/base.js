a0fc6948dcf77d51d56ee1026e74395e77bfc829
0033 <164883814+zhangyw24@users.noreply.github.com>
2025-12-02 08:26:18 +0800

    Refactor comments for clarity in base.js
    修改了注释的格式

--- static/js/controller/base.js
+++ static/js/controller/base.js
@@ -2,19 +2,20 @@
 export class Controller{
     constructor($canvas){
         this.$canvas = $canvas;
-
-        this.pressed_keys = new Set();//存储当时在键盘上按下的键。然后set可以进行去重
+        //存储当时在键盘上按下的键。然后set可以进行去重
+        this.pressed_keys = new Set();
         this.start();
     }
 
 
     start() {
-        let outer = this;//保留了外部的this引用
+        //保留了外部的this引用
+        let outer = this;
         //按键按下。当按键按下时，将按键名添加到pressed_keys集合中
         this.$canvas.keydown(function (e) {
             outer.pressed_keys.add(e.key);
         });
-//按键释放。当按键释放时，从pressed_keys集合中移除对应的键
+        //按键释放。当按键释放时，从pressed_keys集合中移除对应的键
         this.$canvas.keyup(function (e) {
             outer.pressed_keys.delete(e.key);
         });
